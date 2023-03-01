06ac16b1e505e20b87c7b3580d5db132bd6ba35d
Vxcaroda <davidvicaro@gmail.com>
2023-03-01 17:56:45 -0500

    Adding my comments from line 56 to the end -

--- test5.asm
+++ test5.asm
@@ -15,7 +15,7 @@ print_int:
   jr $ra
 
 # Function to print an integer to console, followed by a comma
-print_int_comma:
+print_int_space:
   li $v0, 1
   syscall
   li $a0, ' '
@@ -58,54 +58,53 @@ main:
 	
 
   case2: #Case for if n > 1    
-	blez $t0, exception 			#check if $t0 is less than or equal to zero - if so print exception
-	li $t1, 1						#load integer 1 into register t1
-	bgt $t0, $t1, continue 			#check if $t0 is geater than t1 (1) - if so go to continue:
+	blez $t0, exception 				#check if $t0 is less than or equal to zero - if so print exception
+	li $t1, 1							#load integer 1 into register t1
+	bgt $t0, $t1, continue 				#check if $t0 is geater than t1 (1) - if so go to continue:
 
-continue:								#Continue function  
+continue:								#Continue function definition
   li $t1, 1 # First Fibonacci number	#Initialize Fibonacci sequence first two numbers loading immediate integer 1 into $t1 register 
   li $t2, 1 # Second Fibonacci number	#loading immediate integer 1 into $t2 register 
   la $a0, output						#load the memory address of the string 'output' into the $a0 register
   jal print_string						#jump and link to the print_string function 
 
   # Output first two Fibonacci numbers
-  move $a0, $t1					#moving the value fo $t1 to $a0
-  jal print_int_comma			#jump and link to the print_int_comma function 
-  move $a0, $t2					#moving the value fo $t2 to $a0
-  jal print_int_comma			#jump and link to the print_int_comma function 
+  move $a0, $t1							#moving the value of $t1 into $a0
+  jal print_int_space					#jump and link to the print_int_space function 
+  move $a0, $t2							#moving the value of $a0 into $t2
+  jal print_int_space					#jump and link to the print_int_space function 
 
   # Generate and output remaining Fibonacci numbers
-  sub $t0, $t0, 2 				# Subtract 2 from the value at register $t0 and store result in $t0 
-								# decrement the value of n by 2 because we already outputted first two Fib numbers outside the loop
-  loop:
-    beqz $t0, exit 				# Exit loop when n reaches 0
-	bltz $t0, exit 				# Exit loop for when n < 0
+  sub $t0, $t0, 2 						# Subtract 2 from the value at register $t0 and store result in $t0 
+										# decrement the value of n by 2 because we already outputted first two Fib numbers outside the loop
+  loop:									# Our loop function definition 
+    beqz $t0, exit 						# check if register $t0 is equal to zero - if so then Exit loop
+	bltz $t0, exit 						# check if register $t0 is less than zero - if so then Exit loop
 
     # Calculate next Fibonacci number
-    add $t3, $t1, $t2
-    move $t1, $t2
-    move $t2, $t3
+    add $t3, $t1, $t2					#add the numbers at registers $t1 and $t2 and set register $t3 equal to the sum
+    move $t1, $t2						#moving the value of $t2 into $t1
+    move $t2, $t3						#moving the value of $t3 into $t2
 
     # Output next Fibonacci number, without comma if it's the last number
-    move $a0, $t3
-    addi $t0, $t0, -1
-    beqz $t0, print_int
-    jal print_int_comma
+    move $a0, $t3						#moving the value to $t3 into $a0	
+	sub $t0, $t0, 1						#subtracting 1 from the value stored at $t0, and setting $t0 equal to the difference
+    beqz $t0, print_int					#check if $t0 is equal to zero - if so then call print_int_space function 
+    jal print_int_space					#jump and link to the print_int_space function 
+    j loop								#jump to the loop function to repeat the loop 
 
-    # Decrement n and repeat loop
-    j loop
-
-  exit:
-	la $a0, prompt2
-	li $v0, 4
-	syscall 
-	li $v0, 5
-	move $t0, $v0
-	#check if 0 or 1 or other
-	beqz $t5,terminate
-	li $t6, 1
-	beq $t5, $t6, loop #Pickup where left off from last number grabbed 
+  exit:									#Exit function definition 
+	la $a0, prompt2						#load the memory address of the string 'prompt2' into the $a0 register 
+	li $v0, 4							#load immediate integer 4 into register $v0 for syscall to read 
+	syscall 							#This is system call 4 which is for printing strings 
+	li $v0, 5							#load immediate integer 5 into register $v0
+	move $t0, $v0						#moving the value to $v0 into $t0
+	#check if 0 or 1 or other			#work in progress
+	beqz $t5,terminate					#check if $t5 is equal to zero - if so then call terminatefunction and end the program 
+	li $t6, 1							#load immediate integer 1 into register $t6 
+	beq $t5, $t6, loop 					#check if contents of registers $t5 and $t6 are equal - if so then go to loop function
+							#(comment not complete) We might want to pickup where left off from last number grabbed after the user hits continue 
 	
- terminate:
-    li $v0, 10 # Exit program
-    syscall
+ terminate:								#Our terminate function
+    li $v0, 10 # Exit program			#load immediate integer 10 into register $v0 for syscall to read 
+    syscall								#This is system call 4 which is for terminating execution of the program 
